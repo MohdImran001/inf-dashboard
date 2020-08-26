cfab8e3fe4710f134e5d6abc644fc0c77cb7ed7d
Mohd <imrancrazy200@gmail.com>
2020-08-26 18:46:18 +0530

    all sidebar completed

---
+++ assets/js/app.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+
+    // this code hides and show the status table when load status btn is clicked
+    let f = -1;
+    $('#load-status-btn').click(function() {
+        if(f == -1)
+            $('#load-status-btn').text("HIDE STATUS");
+        else
+            $('#load-status-btn').text("LOAD STATUS");
+
+        f *= -1;
+        $('#status-table').toggle(1000);
+    });
+
+    // this code hides the confirmation-box and shows the confirmed box when the user click yes button
+    $('#yes').click(function() {
+        $('#confirmation-box').hide();
+        $('#confirmed-box').show();
+    });
+
+
+    // this code hides the confirmation-box and shows the rejected-message-box when the user click no button
+
+    $('#no').click(function() {
+        $('#confirmation-box').hide();
+        $('#rejected-message-box').show();
+    });
+
+    // this code hides  the rejected-message-box and show rejection box when the user click submit button
+
+    $('#submit-message').click(function() {
+        $('#rejected-message-box').hide();
+        $('#rejection-box').show();
+    });
+
+});
